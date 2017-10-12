6a91350632c54707a92ecaa1fcedd5de7e474ed2
senamit2708 <senamit2708@gmail.com>
2017-10-12 22:35:50 +0530

    points:
     Dynamic fragment tried first time
    
    Added two fragment to main activity simply and no more functionality inside the fragment till now

--- app/src/main/java/com/example/senamit/fragmentdynamicpractice2/MainActivity.java
+++ app/src/main/java/com/example/senamit/fragmentdynamicpractice2/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.senamit.fragmentdynamicpractice2;
 
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentOneActivity fragment1 = new FragmentOneActivity();
+        FragmentTwoActivity fragment2 = new FragmentTwoActivity();
+
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.main_linear_layout_one, fragment1, "f1");
+        fragmentTransaction.add(R.id.main_linear_layout_two,fragment2,"f2");
+        fragmentTransaction.commit();
+
+
     }
 }
