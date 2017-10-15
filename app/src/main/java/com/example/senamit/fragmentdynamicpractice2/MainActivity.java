ce918284523ab25d8aadf2b5ca05469663243eb4
senamit2708 <senamit2708@gmail.com>
2017-10-15 21:16:09 +0530

    i am able to do the changes from fragment At to replace it by Fragment B

--- app/src/main/java/com/example/senamit/fragmentdynamicpractice2/MainActivity.java
+++ app/src/main/java/com/example/senamit/fragmentdynamicpractice2/MainActivity.java
@@ -5,10 +5,13 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity  implements Communicator{
 
-    FragmentTwoActivity fragment2;
+
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,23 +19,74 @@ public class MainActivity extends AppCompatActivity  implements Communicator{
 
 
 
-        FragmentOneActivity fragment1 = new FragmentOneActivity();
-       fragment2 = new FragmentTwoActivity();
 
 
-        FragmentManager fragmentManager = getFragmentManager();
+
+         fragmentManager = getFragmentManager();
+
+        fragmentAdding();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.add(R.id.main_linear_layout_one, fragment1, "f1");
+//        fragmentTransaction.add(R.id.main_linear_layout_two,fragment2,"f2");
+//        fragmentTransaction.commit();
+
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                FragmentTwoActivity fragment2 = new FragmentTwoActivity();
+                fragmentTransaction.replace(R.id.main_linear_layout_one, fragment2, "f2");
+                fragmentTransaction.commit();
+
+
+            }
+        });
+
+
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                FragmentOneActivity fragment1 = new FragmentOneActivity();
+                fragmentTransaction.replace(R.id.main_linear_layout_one, fragment1);
+                fragmentTransaction.commit();
+
+
+            }
+        });
+
+
+    }
+
+    public void fragmentAdding() {
+
+        FragmentTwoActivity fragment2;
+        FragmentOneActivity fragment1;
+
+        fragment1 = new FragmentOneActivity();
+        fragment2 = new FragmentTwoActivity();
+
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.main_linear_layout_one, fragment1, "f1");
         fragmentTransaction.add(R.id.main_linear_layout_two,fragment2,"f2");
         fragmentTransaction.commit();
-
-
     }
 
     @Override
     public void dataTransfer(int data) {
 
-        fragment2.changeText(data);
+
+
+//        fragment2.changeText(data);
 
     }
-}
+
+
+
+
+
+
+    }
+
